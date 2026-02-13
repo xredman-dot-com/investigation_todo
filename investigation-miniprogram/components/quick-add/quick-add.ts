29e6846dd17a0f46036bec5bfb9098f12a91a979
猫爸 <maoba@dtstars.com>
2026-02-13 14:17:54 +0800

    feat(miniprogram): add task-card and quick-add components

---
+++ investigation-miniprogram/components/quick-add/quick-add.ts
@@ -0,0 +1,25 @@
+// components/quick-add/quick-add.ts
+Component({
+  properties: {
+    placeholder: {
+      type: String,
+      value: '快速添加任务...'
+    }
+  },
+
+  data: {
+    value: ''
+  },
+
+  methods: {
+    onInput(e: WechatMiniprogram.Input) {
+      this.setData({ value: e.detail.value })
+    },
+
+    onConfirm() {
+      if (!this.data.value.trim()) return
+      this.triggerEvent('add', { title: this.data.value })
+      this.setData({ value: '' })
+    }
+  }
+})
---
+++ investigation-miniprogram/components/task-card/task-card.ts
@@ -0,0 +1,27 @@
+// components/task-card/task-card.ts
+Component({
+  properties: {
+    task: {
+      type: Object,
+      value: {}
+    }
+  },
+
+  methods: {
+    onToggle() {
+      this.triggerEvent('toggle', { taskId: this.properties.task.id })
+    },
+
+    onTap() {
+      this.triggerEvent('tap', { taskId: this.properties.task.id })
+    },
+
+    onDetail() {
+      this.triggerEvent('detail', { taskId: this.properties.task.id })
+    },
+
+    onMore() {
+      this.triggerEvent('more', { taskId: this.properties.task.id })
+    }
+  }
+})
